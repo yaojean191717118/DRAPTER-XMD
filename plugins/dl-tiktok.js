052136abdd15b75985e80847e88ecfdac951d395
ⓃⒺCⓉOR🍯 <167437180+drapterlagas@users.noreply.github.com>
2025-04-21 20:49:57 +0300

    Update dl-tiktok.js

--- plugins/dl-tiktok.js
+++ plugins/dl-tiktok.js
@@ -34,8 +34,8 @@ const tiktok = async (m, Matrix) => {
         forwardingScore: 999,
         isForwarded: true,
         forwardedNewsletterMessageInfo: {
-          newsletterJid: "120363290715861418@newsletter",
-          newsletterName: "Popkid",
+          newsletterJid: "0029Vb3zzYJ9xVJk0Y65c81W@newsletter",
+          newsletterName: "ⓃⒺCⓉOR🍯",
           serverMessageId: 143,
         },
       },
