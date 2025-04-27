0d568e70c11dba6249b5df7dd891ac70537c06c3
ⓃⒺCⓉOR🍯 <167437180+drapterlagas@users.noreply.github.com>
2025-04-27 18:56:32 +0300

    Update config.cjs

--- config.cjs
+++ config.cjs
@@ -3,7 +3,7 @@ const fs = require("fs");
 require("dotenv").config();
 
 const config = {
-  SESSION_ID: process.env.SESSION_ID || "nector~14YUhBSJ#eOWaWeoGMpOyvONCz9swFtZ8q24InOPtgV21vPrPKyg",
+  SESSION_ID: process.env.SESSION_ID || "nector~I9oUgKyJ#kOuae8ng6czmJ-SETB4PvcaOIzi5pTNwiH4xn73idLA",
   PREFIX: process.env.PREFIX || '*',
   AUTO_STATUS_SEEN: process.env.AUTO_STATUS_SEEN !== undefined ? process.env.AUTO_STATUS_SEEN === 'true' : true, 
   AUTO_STATUS_REPLY: process.env.AUTO_STATUS_REPLY !== undefined ? process.env.AUTO_STATUS_REPLY === 'true' : true,
